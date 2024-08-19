5d2b166ca59e539691f6966ad4eebd22999c56ad
Samuel Craft <craftsa@bc.edu>
2024-08-19 13:07:51 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    google_id = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    study_sets = db.relationship('StudySet', backref='user', lazy=True)
+
+class StudySet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    cards = db.relationship('FlashCard', backref='study_set', lazy=True)
+
+class FlashCard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False)
+    study_set_id = db.Column(db.Integer, db.ForeignKey('study_set.id'), nullable=False)
+    starred = db.Column(db.Boolean, default=False)
